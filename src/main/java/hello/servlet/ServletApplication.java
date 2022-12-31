4960c4bb90d44bafa92d9a8c7965dbadc611748f
To <tok12225@gmail.com>
2022-12-31 17:32:11 +0900

    mvcWeb Get 방식 before Get방식 코드 작성

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿을 찾아서 등록하는 annotation :: 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
